38d1f6798561547375a3d8c8e4b361d2dc54321f
Nathan <viniciosnathan@gmail.com>
2025-02-07 10:56:14 -0300

    feat: create a get just for tests

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express'; // ES6 syntax
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Server is ready'); // Send message to client
+});
+
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000'); // Log message to console to confirm server is running
 }); 
