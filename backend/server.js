271a06e831a4e446f232b93c2dfc8588ca27404a
Nathan <viniciosnathan@gmail.com>
2025-02-07 10:52:49 -0300

    chore: install nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,7 @@
 import express from 'express'; // ES6 syntax
 
+const app = express();
+
+app.listen(5000, () => {
+    console.log('Server started at http://localhost:5000'); // Log message to console to confirm server is running
+}); 
