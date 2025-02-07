0b1d4e11b3dda24836aa130f962a7e3093962f8f
Nathan <viniciosnathan@gmail.com>
2025-02-07 13:48:28 -0300

    feat: add transaction routes

---
+++ backend/routes/transactionRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'; // Import express module
+const router = express.Router(); // Create router object using express Router method
+import transactionController from '../controllers/transactionController.js'; // Import transactionController object from transactionController.js file
+
+// Define routes
+router.post("/", transactionController.createTransaction);
+router.get("/", transactionController.getTransactions);
+router.get("/:id", transactionController.getTransactionById);
+router.put("/:id", transactionController.updateTransaction);
+router.delete("/:id", transactionController.deleteTransaction);
+
+export default router; // Export router object
