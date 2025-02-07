28b97f506892686e3d578825e9b093c5d2866717
Nathan <viniciosnathan@gmail.com>
2025-02-07 11:19:36 -0300

    feat: add connectDB function call

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
  // ES6 syntax for importing modules
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js'; // Import connectDB function from db.js file
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -14,5 +15,6 @@ console.log(process.env.MONGO_URI);
 
 
 app.listen(5000, () => {
+    connectDB(); // Call connectDB function
     console.log('Server started at http://localhost:5000'); // Log message to console to confirm server is running
 }); 
