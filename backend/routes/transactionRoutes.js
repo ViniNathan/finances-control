8591fe143446f06891ad2b5d8e5a3325d5e5996b
Nathan <viniciosnathan@gmail.com>
2025-02-07 14:57:54 -0300

    feat: add route to remove all transactions from a user

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -7,6 +7,7 @@ router.post("/", transactionController.createTransaction);
 router.get("/", transactionController.getTransactions);
 router.get("/:id", transactionController.getTransactionById);
 router.put("/:id", transactionController.updateTransaction);
+router.delete("/all", transactionController.deleteAllTransactions);
 router.delete("/:id", transactionController.deleteTransaction);
 
 export default router; // Export router object
