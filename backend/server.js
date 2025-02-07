31e85ebe3a18e4d13d2008c0185f082ddd27bc7f
Nathan <viniciosnathan@gmail.com>
2025-02-07 11:11:34 -0300

    feat: import dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
-import express from 'express'; // ES6 syntax
+ // ES6 syntax for importing modules
+import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Server is ready'); // Send message to client
 });
+
+console.log(process.env.MONGO_URI);
+
 
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000'); // Log message to console to confirm server is running
