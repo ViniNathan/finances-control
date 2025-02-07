30c1dc9f3586a696446d7fed54a8875342d6f9b8
Nathan <viniciosnathan@gmail.com>
2025-02-07 11:20:39 -0300

    feat: remove console log just used for tests

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,6 @@ app.get('/', (req, res) => {
     res.send('Server is ready'); // Send message to client
 });
 
-console.log(process.env.MONGO_URI);
-
-
 app.listen(5000, () => {
     connectDB(); // Call connectDB function
     console.log('Server started at http://localhost:5000'); // Log message to console to confirm server is running
