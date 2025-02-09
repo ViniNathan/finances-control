a5e247e4f2bdfeb65dec21a8d640db76ba1d8df4
Nathan <viniciosnathan@gmail.com>
2025-02-09 18:07:56 -0300

    feat: add auth middleware to protect transaction routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+
+export const protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Header token
+            token = req.headers.authorization.split(' ')[1];
+
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // User token
+            req.user = await User.findById(decoded.id).select('-passwordHash');
+
+            next();
+        } catch (error) {
+            res.status(401).json({ message: 'Unauthorized, token failed' });
+        }
+    }
+
+    if (!token) {
+        res.status(401).json({ message: 'Unauthorized, no token' });
+    }
+};
