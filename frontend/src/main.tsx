13dc3d0c0e03cfeb0f166ec84bb5acd19af1b0b6
Nathan <viniciosnathan@gmail.com>
2025-02-10 13:14:13 -0300

    feat: add dashboard route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter,  Routes, Route } from "react-router";
 import MainPage from "./routes/MainPage";
 import Login from './routes/Login';
+import Dashboard from './routes/Dashboard';
 import './index.css'
 
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
   </BrowserRouter>,
 )
---
+++ frontend/src/routes/Dashboard.tsx
@@ -0,0 +1,10 @@
+
+const Dashboard = () => {
+  return (
+    <div className="h-screen w-screen">
+        <h1 className="text-4xl text-white text-center pt-20">Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
