efb839b79842309c7b752c31e544c8cbe2df706f
Nathan <viniciosnathan@gmail.com>
2025-02-11 16:12:01 -0300

    feat: refactor login page

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -20,21 +20,23 @@ const Login = () => {
     };
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-stone-200 py-20">
+    <div className="h-screen w-screen flex justify-center items-center bg-bg py-20">
         <div className="flex flex-col justify-center items-center gap-10">
-            <div className="text-4xl text-center">
-                Welcome back! Glad to see you.
+            <div className="text-6xl text-center font-bold text-primary">FinTrack</div>
+            <div className="flex flex-col justify-center items-center gap-1">
+              <div className="text-4xl text-center font-semibold">Welcome back!</div>
+              <div className="text-4xl text-center font-semibold"> Glad to see you.</div>
             </div>
-            <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+            <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6 w-[90vw] md:w-[30rem]">
               {error && (
-                <div className="bg-red-500/10 border border-red-500 text-black px-4 py-3 rounded-lg text-sm">
+                <div className="bg-red-500/10 border border-red-500 text-black px-4 py-3 rounded-3xl text-sm">
                   {error}
                 </div>
               )}
               
               <div className="space-y-4">
                 <div>
-                  <label htmlFor="email" className="block text-sm font-medium text-gray-800 mb-1">
+                  <label htmlFor="email" className="block text-sm font-medium text-gray-800 mb-1 ml-4">
                     Email
                   </label>
                   <input
@@ -42,8 +44,8 @@ const Login = () => {
                     type="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="w-full px-4 py-2 bg-white/5 border border-gray-800 text-black rounded-lg 
-                             focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent
+                    className="w-full px-4 py-2 bg-secondary text-black rounded-3xl 
+                             focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent
                              placeholder-gray-400 transition-all"
                     placeholder="Enter your email"
                     required
@@ -51,7 +53,7 @@ const Login = () => {
                 </div>
 
                 <div>
-                  <label htmlFor="password" className="block text-sm font-medium text-gray-800 mb-1">
+                  <label htmlFor="password" className="block text-sm font-medium text-gray-800 mb-1 ml-4">
                     Password
                   </label>
                   <input
@@ -59,8 +61,8 @@ const Login = () => {
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="w-full px-4 py-2 bg-white/5 border border-gray-800 text-black rounded-lg 
-                             focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent
+                    className="w-full px-4 py-2 bg-secondary text-black rounded-3xl 
+                             focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent
                              placeholder-gray-400 transition-all"
                     placeholder="Enter your password"
                     required
@@ -71,8 +73,8 @@ const Login = () => {
               <button
                 type="submit"
                 disabled={loading}
-                className="w-full py-2.5 sm:py-3 px-4 bg-stone-900 hover:bg-stone-700 text-white font-medium 
-                         rounded-lg transition-colors duration-200 focus:outline-none focus:ring-2 
+                className="w-full py-2.5 sm:py-3 px-4 bg-primary hover:bg-dark-green text-dark hover:text-bg font-semibold  text-lg
+                         rounded-3xl transition-colors duration-200 focus:outline-none focus:ring-2 
                          focus:ring-offset-2 focus:ring-stone-500 disabled:opacity-50 
                          disabled:cursor-not-allowed disabled:hover:bg-stone-600 cursor-pointer"
               >
@@ -89,8 +91,8 @@ const Login = () => {
                 )}
               </button>
             </form>
-            <div className="text-sm">
-                Don't have an account? <a href="/register">Register now</a>
+            <div className="text-md">
+                Don't have an account? <a href="/register"><span className="text-dark-green font-bold">Register now</span></a>
             </div>
         </div>
     </div>
