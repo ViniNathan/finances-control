2cb4596dcf758047ab76018ec04412142a90898f
Nathan <viniciosnathan@gmail.com>
2025-02-11 20:08:15 -0300

    feat: update returnButton to return just to homepage

--- frontend/src/components/ReturnButton.tsx
+++ frontend/src/components/ReturnButton.tsx
@@ -7,7 +7,7 @@ const ReturnButton = () => {
   return (
     <div className="absolute top-4 left-4">
         <button 
-          onClick={() => navigate(-1)} 
+          onClick={() => navigate("/")} 
           className="flex items-center rounded-3xl"
         >
             <MdKeyboardReturn className="size-9 md:size-10 text-primary border-2 border-primary rounded-lg hover:border-dark-green hover:text-bg hover:bg-dark-green transition-all ease-in-out"/>
