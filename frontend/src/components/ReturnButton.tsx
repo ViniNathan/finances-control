dfe057d5692eae6851bb3cd38f7d002300bdc1d5
Nathan <viniciosnathan@gmail.com>
2025-02-11 19:50:47 -0300

    feat: update Return Button UI and add functionality

--- frontend/src/components/ReturnButton.tsx
+++ frontend/src/components/ReturnButton.tsx
@@ -1,8 +1,18 @@
-
+import { MdKeyboardReturn } from "react-icons/md";
+import { useNavigate } from "react-router";
 
 const ReturnButton = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>ReturnButton</div>
+    <div className="absolute top-4 left-4">
+        <button 
+          onClick={() => navigate(-1)} 
+          className="flex items-center rounded-3xl"
+        >
+            <MdKeyboardReturn className="size-9 md:size-10 text-primary border-2 border-primary rounded-lg hover:border-dark-green hover:text-bg hover:bg-dark-green transition-all ease-in-out"/>
+        </button>
+    </div>
   )
 }
 
