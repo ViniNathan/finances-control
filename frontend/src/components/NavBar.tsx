c1a7b921bd10c1db03a644bac98b9b3409f993ef
Nathan <viniciosnathan@gmail.com>
2025-02-12 18:43:03 -0300

    feat: add NavbarProps

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,24 @@
-import { GrHistory, GrMoney } from "react-icons/gr";
+import { GrHistory} from "react-icons/gr";
+import { FaMoneyBillTransfer } from "react-icons/fa6";
 
-const Navbar = () => {
+interface NavbarProps {
+    isAdding: boolean;
+    setIsAdding: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Navbar : React.FC<NavbarProps> = ({ isAdding, setIsAdding }) => {
   return (
     <div className="absolute bottom-0 bg-secondary w-screen flex justify-center items-center p-4 rounded-tr-4xl rounded-tl-4xl gap-10 shadow-[0_-5px_10px_rgba(14,62,62,0.3)]">
-        <GrHistory className="size-7 text-dark-green" />
-        <GrMoney className="size-7 text-dark-green" />
+      <GrHistory 
+        className={`size-7 ${!isAdding ? "text-dark-green" : "text-gray-400"} cursor-pointer`}
+        onClick={() => setIsAdding(false)}
+      />
+      <FaMoneyBillTransfer 
+        className={`size-7 ${isAdding ? "text-dark-green" : "text-gray-400"} cursor-pointer`}
+        onClick={() => setIsAdding(true)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
