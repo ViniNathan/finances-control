ba51903929eeab7ab1218134d271d2455066cb77
Nathan <viniciosnathan@gmail.com>
2025-02-13 16:13:30 -0300

    feat: implement NewTransaction component with form handling and state management

--- frontend/src/components/NewTransaction.tsx
+++ frontend/src/components/NewTransaction.tsx
@@ -1,11 +1,32 @@
 import { useState } from "react";
+import useTransactions from "../hooks/useTransactions";
 
 interface NewTransactionProps {
   setIsAdding: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const NewTransaction: React.FC<NewTransactionProps> = ({ setIsAdding }) => {
-  const [transactionType, setTransactionType] = useState<"Income" | "Expense">("Income");
+const NewTransaction: React.FC<NewTransactionProps> = () => {
+  const [transactionType, setTransactionType] = useState<"income" | "expense">("income");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const { createTransaction } = useTransactions();
+
+  const handleAddTransaction = async (e: any) => {
+    e.preventDefault();
+    try {
+      await createTransaction({
+        type: transactionType,
+        category,
+        amount,
+        date,
+        description,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="w-full max-w-md mx-auto px-6 md:px-8">
@@ -13,50 +34,58 @@ const NewTransaction: React.FC<NewTransactionProps> = ({ setIsAdding }) => {
         Add Transaction
       </h2>
       
-      <form className="w-full flex flex-col gap-4">
+      <form className="w-full flex flex-col gap-4" onSubmit={handleAddTransaction}>
         <div className="flex justify-center mb-6">
           <div className="flex flex-wrap gap-3">
             <button
               type="button"
               className={`px-4 sm:px-6 py-2 rounded-full font-medium transition-all whitespace-nowrap
-                ${transactionType === "Income" ? "bg-primary text-dark shadow-md" : "bg-secondary text-gray-600"}
+                ${transactionType === "income" ? "bg-primary text-dark shadow-md" : "bg-secondary text-gray-600"}
               `}
-              onClick={() => setTransactionType("Income")}
+              onClick={() => setTransactionType("income")}
             >
               Income
             </button>
             <button
               type="button"
               className={`px-4 sm:px-6 py-2 rounded-full font-medium transition-all whitespace-nowrap
-                ${transactionType === "Expense" ? "bg-dark-green text-bg shadow-md" : "bg-secondary text-gray-600"}
+                ${transactionType === "expense" ? "bg-dark-green text-bg shadow-md" : "bg-secondary text-gray-600"}
               `}
-              onClick={() => setTransactionType("Expense")}
+              onClick={() => setTransactionType("expense")}
             >
               Expense
             </button>
           </div>
         </div>
-
-        <input 
-          className="w-full bg-secondary p-3 rounded-3xl text-sm text-gray-800 placeholder-gray-500" 
-          type="text" 
-          placeholder="Category" 
-        />
-        
-        <input 
-          className="w-full bg-secondary p-3 rounded-3xl text-sm text-gray-800 placeholder-gray-500" 
-          type="number" 
-          placeholder="Amount" 
-        />
-        
+        <div className="flex flex-row gap-1">
+          <input 
+            className="w-full bg-secondary p-3 rounded-3xl text-sm text-gray-800 placeholder-gray-500" 
+            type="text" 
+            placeholder="Category" 
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          
+          <input 
+            className="w-full bg-secondary p-3 rounded-3xl text-sm text-gray-800 placeholder-gray-500" 
+            type="number" 
+            placeholder="Amount" 
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+        </div>
         <input 
           className="w-full bg-secondary p-3 rounded-3xl text-sm text-gray-800 placeholder-gray-500" 
           type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
         />
         
         <textarea 
           className="w-full bg-secondary p-3 rounded-3xl text-sm text-gray-800 placeholder-gray-500 h-[100px] text-left align-text-top resize-none" 
           placeholder="Description" 
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
 
         <div className="flex flex-col sm:flex-row gap-3 sm:gap-4 mt-4">
