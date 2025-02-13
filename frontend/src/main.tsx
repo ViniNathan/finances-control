96730b84f5e4b0270e45becc143d1b9ccbba4f12
Nathan <viniciosnathan@gmail.com>
2025-02-13 10:23:33 -0300

    feat: add private-route prop to /dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter,  Routes, Route } from "react-router";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
 import MainPage from "./routes/MainPage";
 import Login from './routes/Login';
 import Register from './routes/Register';
 import Dashboard from './routes/Dashboard';
-import './index.css'
-
+import PrivateRoute from './routes/PrivateRoute';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<PrivateRoute />}>
         <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      </Route>
+      
+    </Routes>
   </BrowserRouter>,
-)
+);
