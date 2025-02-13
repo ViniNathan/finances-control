d8bafd4211d02e93851151f052987c4b700e9468
Nathan <viniciosnathan@gmail.com>
2025-02-13 08:50:57 -0300

    feat: update Dashboard to add a button to scroll up and down the view

--- frontend/src/routes/Dashboard.tsx
+++ frontend/src/routes/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { ChevronUp, ChevronDown } from "lucide-react";
 import Topbar from "../components/Topbar";
 import GeneralProperties from "../components/GeneralProperties";
 import DateFilter from "../components/DateFilter";
@@ -10,12 +11,17 @@ const Dashboard = () => {
   const generalRef = useRef<HTMLDivElement>(null);
   const [generalHeight, setGeneralHeight] = useState(0);
   const [isAdding, setIsAdding] = useState(false);
+  const [isPanelExpanded, setIsPanelExpanded] = useState(false);
 
   useEffect(() => {
     if (generalRef.current) {
       setGeneralHeight(generalRef.current.clientHeight);
     }
   }, []);
+
+  const togglePanel = () => {
+    setIsPanelExpanded(!isPanelExpanded);
+  };
 
   return (
     <div className="min-h-screen w-screen bg-primary flex flex-col">
@@ -24,10 +30,21 @@ const Dashboard = () => {
         <GeneralProperties />
       </div>
       <div
-        className="absolute bottom-0 w-screen flex justify-center items-start bg-bg rounded-tl-4xl rounded-tr-4xl p-5 overflow-auto shadow-[0_-5px_10px_rgba(14,62,62,0.3)]"
-        style={{ top: `${generalHeight + 64}px` }}
+        className="absolute bottom-0 w-screen flex flex-col justify-start items-center bg-bg rounded-tl-4xl rounded-tr-4xl p-5 overflow-auto shadow-[0_-5px_10px_rgba(14,62,62,0.3)]"
+        style={{ top: isPanelExpanded ? "65px" : `${generalHeight + 64}px` }}
       >
-        <div className="w-screen flex flex-col justify-start items-center">
+        <button
+          onClick={togglePanel}
+          className="absolute top-0 left-1/2 transform -translate-x-1/2"
+        >
+          {isPanelExpanded ? (
+            <ChevronDown className="size-10 text-gray-600" />
+          ) : (
+            <ChevronUp className="size-10 text-gray-600" />
+          )}
+        </button>
+        
+        <div className="w-full flex flex-col justify-start items-center mt-5 ">
           {!isAdding ? (
             <>
               <DateFilter />
