0e5e03a77312c74c3e210be40bb74e58063beba0
Nathan <viniciosnathan@gmail.com>
2025-02-13 19:53:10 -0300

    feat: improve font sizes and layout for balance, income, and expense display in GeneralProperties component

--- frontend/src/components/GeneralProperties.tsx
+++ frontend/src/components/GeneralProperties.tsx
@@ -44,23 +44,23 @@ const GeneralProperties = () => {
   };
 
   return (
-    <div className="h-max flex flex-row justify-center items-center gap-6 my-10 md:gap-10 md:mt-15">
+    <div className="h-max flex flex-row justify-center items-center gap-4 my-10 md:gap-10 md:mt-15">
       <div className="flex flex-col justify-center items-center gap-1">
         <GiTakeMyMoney className="size-7 md:size-10"/>
-        <div className="text-dark font-semibold text-xs md:text-lg">Total Balance</div>
-        <div className="text-bg font-bold text-2xl">{formatCurrency(balance)}</div>
+        <div className="text-dark font-semibold text-md md:text-lg">Total Balance</div>
+        <div className="text-bg font-bold text-lg md:text-2xl">{formatCurrency(balance)}</div>
       </div>
       <div className="h-20 w-[0.1px] bg-bg"></div>
       <div className="flex flex-col justify-center items-center gap-1">
         <GiReceiveMoney className="size-7 md:size-10"/>
-        <div className="text-dark font-semibold text-xs md:text-lg">Income</div>
-        <div className="text-bg font-bold text-2xl">{formatCurrency(income)}</div>
+        <div className="text-dark font-semibold text-md md:text-lg">Income</div>
+        <div className="text-bg font-bold text-lg md:text-2xl">{formatCurrency(income)}</div>
       </div>
       <div className="h-20 w-[0.1px] bg-bg"></div>
       <div className="flex flex-col justify-center items-center gap-1">
         <GiPayMoney className="size-7 md:size-10"/>
-        <div className="text-dark font-semibold text-xs md:text-lg">Expense</div>
-        <div className="text-bg font-bold text-2xl">{formatCurrency(expense)}</div>
+        <div className="text-dark font-semibold text-md md:text-lg">Expense</div>
+        <div className="text-bg font-bold text-lg md:text-2xl">{formatCurrency(expense)}</div>
       </div>
     </div>
   );
