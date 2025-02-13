20361f00a2c006c445c38c0e800601b73372d019
Nathan <viniciosnathan@gmail.com>
2025-02-13 13:10:45 -0300

    feat: update Register component to format date and capitalize strings

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -7,25 +7,42 @@ interface RegisterProps {
     category: string;
 };
 
-const Register: React.FC<RegisterProps> =  ({type, amount, date, category}) => {
+const Register: React.FC<RegisterProps> = ({ type, amount, date, category }) => {
   const handleType = (type: string) => {
-    if (type == "Income") {
-      return <div className="flex justify-center items-center h-10 w-10 bg-primary rounded-[100%]"><FaArrowTrendUp className="size-5 text-dark-green" /></div>;
+    if (type === "income") {
+      return (
+        <div className="flex justify-center items-center h-10 w-10 bg-primary rounded-[100%]">
+          <FaArrowTrendUp className="size-5 text-dark-green" />
+        </div>
+      );
     } else {
-      return <div className="flex justify-center items-center h-10 w-10 bg-dark-green rounded-[100%]"><FaArrowTrendDown className="size-5 text-bg" /></div>;
-  }};
+      return (
+        <div className="flex justify-center items-center h-10 w-10 bg-dark-green rounded-[100%]">
+          <FaArrowTrendDown className="size-5 text-bg" />
+        </div>
+      );
+    }
+  };
+
+  const formatDate = (dateString: string) => {
+    return new Intl.DateTimeFormat("en-US", {
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit",
+    }).format(new Date(dateString));
+  };
 
   return (
     <div className="my-4 py-4 flex flex-row justify-between items-center">
       {handleType(type)}
       <div className="flex flex-col justify-center items-start">
-        <div className="text-xs font-bold text-dark-green">{date}</div>
-        <div className="text-lg font-semibold">{type}</div>
-        <div className="text-lg text-dark font-bold">{category}</div>
+        <div className="text-xs font-bold text-dark-green">{formatDate(date)}</div>
+        <div className="text-lg font-semibold capitalize">{type}</div>
+        <div className="text-lg text-dark font-bold capitalize">{category}</div>
       </div>
       <div className="text-2xl font-bold">${amount}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
