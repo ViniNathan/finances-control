c779e0f5c008169b4e2bcb5d9285e23943ec33ba
Nathan <viniciosnathan@gmail.com>
2025-02-14 09:01:21 -0300

    feat: configure server for production environment and add cross-env for script management

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
  // ES6 syntax for importing modules
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import cors from 'cors';
 import { connectDB } from './config/db.js'; // Import connectDB function from db.js file
 import transactionRoutes from './routes/transactionRoutes.js'; // Import transactionRoutes object from transactionRoutes.js file
@@ -8,7 +9,10 @@ import authRoutes from './routes/authRoutes.js'; // Import authRoutes object fro
 
 dotenv.config(); // Load environment variables from .env file
 
+const __dirname = path.resolve(); // Define the current directory
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 connectDB(); // Call connectDB function
 app.use(express.json()); // Middleware to parse JSON data in request body
 app.use(cors()); // Middleware to enable CORS
@@ -21,8 +25,12 @@ app.get('/', (req, res) => {
 app.use('/api/transactions', transactionRoutes); // Routes for transactions
 app.use('/auth', authRoutes); // Routes for user authentication
 
-// Porta configurável pelo .env
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log('Server started at http://localhost:5000'); // Log message to console to confirm server is running
