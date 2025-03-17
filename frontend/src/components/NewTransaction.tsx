4c72f84c03a07fc1388325465e828237e810cc0c
Nathan <viniciosnathan@gmail.com>
2025-03-17 13:39:31 -0300

    fix: remove unused authentication hook from NewTransaction component

--- frontend/src/components/NewTransaction.tsx
+++ frontend/src/components/NewTransaction.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import useTransactions from "../hooks/useTransactions";
 import TransactionForm from "./TransactionForm";
-import { useAuth } from "../hooks/useAuth";
 
 interface NewTransactionProps {
   setIsAdding: React.Dispatch<React.SetStateAction<boolean>>;
