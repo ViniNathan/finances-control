1a9fe12051f76dfd317aa2bcb2e48986c901192c
Nathan <viniciosnathan@gmail.com>
2025-03-27 09:04:26 -0300

    fix: wrap main application routes with fragment and update Analytics component import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,25 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router";
+import { Analytics } from "@vercel/analytics/react"
 import MainPage from "./routes/MainPage";
 import Login from './routes/Login';
 import Register from './routes/Register';
 import Dashboard from './routes/Dashboard';
 import PrivateRoute from './routes/PrivateRoute';
-import { Analytics } from "@vercel/analytics/react"
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Analytics/>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route element={<PrivateRoute />}>
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <>
+    <Analytics />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
 );
